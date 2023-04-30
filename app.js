6be87ea012a368cfee49dea02a3dad29c7b6f0b8
Ike-Ikon <asieduisaac5775@gmail.com>
2023-04-30 01:19:04 +0000

    node connet to mongodb

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongodb = require('./db/connect');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app
+//   .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  })
+  .use('/', require('./routes'));
+
+mongodb.initDb((err, db) => {
+  if (err) {
+    console.log(err);
+  }else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+  }
+});
--- controllers/index.js
+++
@@ -1,9 +0,0 @@
-const helloFunction = (req, res, next) => {
-    res.json("Joseph Asante");
-  };
-
-const anotherPerson = (req, res, next) => {
-    res.json("Isaac Asiedu");
-  };
-
-module.exports = {helloFunction, anotherPerson};

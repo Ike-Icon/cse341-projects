2cf38eaa688c0f26e7e74ddaa76d1b9a8018e30b
Ike-Ikon <asieduisaac5775@gmail.com>
2023-05-01 12:43:09 +0000

    mongo is now connected and working

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
+
 
 app
-//   .use(bodyParser.json())
+  .use(bodyParser.json())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     next();
--- controllers/contacts.js
+++ controllers/contacts.js
@@ -2,7 +2,7 @@ const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res, next) => {
-  const result = await mongodb.getDb().db().collection('contacts').find();
+  const result = await mongodb.getDb().db('cse341').collection('contacts').find();
   result.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists);
@@ -10,16 +10,18 @@ const getAll = async (req, res, next) => {
 };
 
 const getSingle = async (req, res, next) => {
-  const userId = new ObjectId(req.params.id);
-  const result = await mongodb
-    .getDb()
-    .db()
-    .collection('contacts')
-    .find({ _id: userId });
-  result.toArray().then((lists) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(lists[0]);
-  });
+  try {
+    const userId = new ObjectId(req.params.id);
+    const result = await mongodb.getDb().db('cse341').collection('contacts').findOne({ _id: userId });
+    if (!result) {
+      res.status(404).json({ message: 'Object not found' });
+    } else {
+      res.status(200).json(result);
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
+  }
 };
 
 module.exports = { getAll, getSingle};

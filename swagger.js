50d47aa252d6fb1aa250ba690957f6054662b328
Ike-Ikon <asieduisaac5775@gmail.com>
2023-05-14 21:18:45 +0000

    swagger not working

--- swagger.js
+++ swagger.js
@@ -3,18 +3,17 @@ const swaggerAutogen = require('swagger-autogen')();
 const doc = {
   info: {
     title: 'My API',
-    description: 'Contacts API'
+    description: 'Contacts API',
+    version: '1.0.0' // Add a version number
   },
   host: 'cse341-projects-oalm.onrender.com',
   schemes: ['https']
 };
 
 const outputFile = './swagger.json';
-const endpointFile = ['./routes/index.js'];
+const endpointFiles = ['./routes/index.js']; // Use an array of endpoint files if you have multiple files
 
-swaggerAutogen(outputFile, endpointFile, doc);
-
-// Run server after it gets generated
-swaggerAutogen(outputFile, endpointFile, doc).then(async () => {
-  await import('./app.js');
+swaggerAutogen(outputFile, endpointFiles, doc).then(() => {
+  // Import and start your server here
+  require('./app');
 });
